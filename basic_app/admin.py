98f75707c720a82c7b51a1c1f86311ea8d2b97e3
faiz ahmed <faizahmed11234@gmail.com>
2019-04-10 14:33:40 +0500

    Adding all Scholl app files

---
+++ basic_app/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from basic_app.models import Student, School
+
+admin.site.register(School)
+admin.site.register(Student)
---
+++ basic_app/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import (View,TemplateView,
+                                  ListView,DetailView,
+                                  CreateView,UpdateView,
+                                  DeleteView)
+
+from . import models
+
+class CBview(TemplateView):
+    template_name = 'index.html'
+
+
+class SchoolListView(ListView):
+    model = models.School
+    context_object_name = 'schools'
+    template_name = 'basic_app/school_list.html'
+
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+    context_object_name = 'school_detail'
+
+class SchoolCreateView(CreateView):
+    fields = ('name','principal','location')
+    model = models.School
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name','principal')
+    model = models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy('basic_app:list')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # this is a comment
